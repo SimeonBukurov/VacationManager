c81de58f451982801ad4a93a3cf6d015f623be35
СИМЕОН БУКУРОВ <sb45963079@edu.mon.bg>
2021-12-07 17:40:20 +0200

    UserMenu

---
+++ CRUDOperations/UserMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using DataModels;
+
+namespace CRUDOperations
+{
+    public class UserMenu
+    {
+         static void CreateUser(ModelContext db)
+        {
+            Console.Write("User name: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+            if (username == " " || password == " ")
+            {
+                Console.WriteLine("Canceled!");
+            }
+            else
+            {
+                Console.Write("Personal Name: ");
+                string personalName = Console.ReadLine();
+                User user = new User();
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+
+        }
+    }
+
+}
