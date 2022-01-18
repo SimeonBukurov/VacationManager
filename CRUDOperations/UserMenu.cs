df08ddeca298dce2180c6f356a2708d5afdff50d
СИМЕОН БУКУРОВ <sb45963079@edu.mon.bg>
2022-01-18 23:18:44 +0200

    Polished CRUD

--- CRUDOperations/UserMenu.cs
+++ CRUDOperations/UserMenu.cs
@@ -18,7 +18,7 @@ namespace CRUDOperations
             Console.WriteLine();
             Console.Write("Enter your command: ");
             string input = Console.ReadLine();
-
+            Console.WriteLine();
             int selection = -1;
             if (int.TryParse(input, out selection))
             {
@@ -46,45 +46,73 @@ namespace CRUDOperations
             string username = Console.ReadLine();
             Console.Write("Password: ");
             string password = Console.ReadLine();
-            if (username == " " || password == " ")
+            if (username == null || password == null)
             {
-                Console.WriteLine("Canceled!");
+                throw new Exception("Canceled!");
             }
             else
             {
                 Console.Write("Personal Name: ");
                 string personalName = Console.ReadLine();
-                string teaminput = Console.ReadLine();                   
-                Team teamItem = db.Teams.FirstOrDefault(team => team.Name == teaminput);
-                if (teamItem != null)
+                string teaminput = Console.ReadLine();
+                if (personalName == null || teaminput == null)
                 {
-                    Console.Write("Enter team name to join:");
-                    string teamname = Console.ReadLine();
-                    teamItem.Name = teamname;
-                    db.SaveChanges();
-                    Console.WriteLine("Team Joined");
+                    throw new Exception("PersonalName and teamInput are not found!");
                 }
                 else
                 {
-                    Console.WriteLine("Team not found!");
+                    Team teamItem = db.Teams.FirstOrDefault(team => team.Name == teaminput);
+                    if (teamItem != null)
+                    {
+                        Console.Write("Enter team name to join:");
+                        string teamname = Console.ReadLine();
+                        if (teamname == null)
+                        {
+                            throw new Exception("TeamName is not found!");
+
+                        }
+                        else 
+                        { 
+                            teamItem.Name = teamname;
+                            db.SaveChanges();
+                            Console.WriteLine("Team Joined");
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Team not found!");
+                    }
+                    string roleinput = Console.ReadLine();
+                    if (roleinput != null)
+                    {
+                        Role roleItem = db.Roles.FirstOrDefault(role => role.Name == roleinput);
+                        if (roleItem != null)
+                        {
+                            Console.Write("Enter role name to join:");
+                            string rolename = Console.ReadLine();
+                            if (rolename != null)
+                            {
+                                roleItem.Name = rolename;
+                                db.SaveChanges();
+                                Console.WriteLine("Entered Role");
+                            }
+                            else
+                            {
+                                throw new Exception("Not entered Role name");
+                            }
+                            User user = new User(username, password, personalName, roleItem, teamItem);
+                            db.Users.Add(user);
+                            db.SaveChanges();
+                        }
+                        else
+                        {
+                            throw new Exception("Role not found!");
+                        }
+                        
+                    }
                 }
-                string roleinput = Console.ReadLine();
-                Role roleItem = db.Roles.FirstOrDefault(role => role.Name == roleinput);
-                if (roleItem != null)
-                {
-                    Console.Write("Enter role name to join:");
-                    string rolename = Console.ReadLine();
-                    roleItem.Name = rolename;
-                    db.SaveChanges();
-                    Console.WriteLine("Entered Role");
-                }
-                else
-                {
-                    Console.WriteLine("Role not found!");
-                }
-                User user = new User(username, password, personalName, roleItem, teamItem);                  
-                db.Users.Add(user);
-                db.SaveChanges();
+ 
+
             }
 
         }
@@ -106,30 +134,41 @@ namespace CRUDOperations
             User usernameresearch = db.Users.FirstOrDefault(User => User.Username == input);
                 if (usernameresearch == null)
                 {
-                    Console.Write("User Not Found!");
+                    throw new Exception("User Not Found!");
                 }
                 else
                 {
                     Console.Write("Enter new username: ");
                     string name = Console.ReadLine();
-
-                    usernameresearch.Username = name;
-                    db.SaveChanges();
-                    Console.Write("User Updated!");
-                }
+                    if (name == null)
+                    {
+                        throw new Exception("Name Is Not Found!");
+                    }
+                else { 
+                        usernameresearch.Username = name;
+                        db.SaveChanges();
+                        Console.Write("User Updated!");
+                     }
+                 }
             User personalnameresearch = db.Users.FirstOrDefault(User => User.PersonalName == input);
             if (personalnameresearch == null)
             {
-                Console.Write("User Not Found!");
+                throw new Exception("User Not Found!");
             }
             else
             {
                 Console.Write("Enter new username: ");
                 string name = Console.ReadLine();
-
-                personalnameresearch.Username = name;
-                db.SaveChanges();
-                Console.Write("User Updated!");
+                if (name == null)
+                {
+                    throw new Exception("Name Is Not Found!");
+                }
+                else
+                { 
+                    personalnameresearch.Username = name;
+                    db.SaveChanges();
+                    Console.Write("User Updated!");
+                }
             }
         }
         static void RemoveUser(ModelContext db)
